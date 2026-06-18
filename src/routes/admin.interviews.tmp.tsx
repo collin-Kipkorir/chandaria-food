bd45f9104a43f89af82e6a02df4e9e87cd12ab88
metacodeltd <collinkipkorir@gmail.com>
2026-06-18 20:14:15 +0300

    Restore interviews page; fix corrupted helper files; users listing verified

--- src/routes/admin.interviews.tmp.tsx
+++ src/routes/admin.interviews.tmp.tsx
@@ -1,6 +1,9 @@
 export default function InterviewsPageTemp() {
   return <div className="p-4">Interviews placeholder (temp)</div>;
 }
+export default function InterviewsPageTemp() {
+  return <div className="p-4">Interviews placeholder (temp)</div>;
+}
 export default function InterviewsPage() {
   return <div className="p-4">Interviews placeholder</div>;
 }
